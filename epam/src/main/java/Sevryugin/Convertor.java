ad1522ac14d7f45b567744b3b8330063a4e66169
misa130 <misasevervv@mail.ru>
2020-03-03 17:09:07 +0400

    обновил метод convert<T,V> класса Convertor<T,V>

--- epam/src/main/java/Sevryugin/Convertor.java
+++ epam/src/main/java/Sevryugin/Convertor.java
@@ -3,17 +3,21 @@ package Sevryugin;
 import lombok.Data;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Parameter;
 
 @Data
 public class Convertor<T, V> {
-    public void convert(T object) throws ClassNotFoundException {
-        V v = (V) new Object();
-        T t = (T) new Object();
-        String c = v.getClass().getName();
-        Class clazz = object.getClass();
-        System.out.println(c);
-    for(Field field : clazz.getDeclaredFields())
-        System.out.println(field);
+    public void convert(T t, V v) throws IllegalAccessException {
+        System.out.println(t.getClass());
+        System.out.println(v.getClass());
+        Class classT = t.getClass();
+        Field[] fields = classT.getDeclaredFields();
+        for(Field field : fields){
+            String fieldName = field.getName();
+            if(!field.isAccessible()) field.setAccessible(true);
+            Object value = field.get(t);// значение поля
+            System.out.println(field +"\t -> "+fieldName+"\t -> "+value);
+        }
         //return v;
     }
 }
--- epam/src/main/java/Sevryugin/Service.java
+++ epam/src/main/java/Sevryugin/Service.java
@@ -1,12 +1,14 @@
 package Sevryugin;
 
 public class Service {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException {
         Human human =new Human();
         human = human.createRandomHuman();
         DtoHuman dtoHuman = new DtoHuman();
         Convertor<Human, DtoHuman> convertor = new Convertor<>();
+        System.out.println(human);
+        System.out.println();
         //dtoHuman =
-                convertor.convert(human);
+                convertor.convert(human, dtoHuman);
     }
 }
