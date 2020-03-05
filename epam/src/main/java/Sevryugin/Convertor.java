854c5ecb4d0e6cf9a576c9dade470f3c22ad768e
misa130 <misasevervv@mail.ru>
2020-03-05 16:51:01 +0400

    .gitignore: убрал resourses из игнора убрал tests из игнора Convertor: убрал методы, которые конвертировали массивы сущностей переназвал методы конвертации одиночных сущностей Human и DtoHuman: убрал поле дефолтных значений убрал метод генерации полей объекта Service: добавил метод генерации одного объекта dtoHuman

--- epam/src/main/java/Sevryugin/Convertor.java
+++ epam/src/main/java/Sevryugin/Convertor.java
@@ -1,10 +1,12 @@
 package Sevryugin;
+
 import lombok.Data;
+
 @Data
 public class Convertor {
     public Human convertToHuman(DtoHuman dtoHuman) {
         Human human;
-        human = new Human(dtoHuman.getId(),dtoHuman.getName(), dtoHuman.getBirthDate().getYear(), dtoHuman.getBirthDate().getMonth(),
+        human = new Human(dtoHuman.getId(), dtoHuman.getName(), dtoHuman.getBirthDate().getYear(), dtoHuman.getBirthDate().getMonth(),
                 dtoHuman.getBirthDate().getDate(), dtoHuman.getDtoAddress().getHouseNumber(), dtoHuman.getDtoAddress().getStreet(),
                 dtoHuman.getDtoAddress().getApartmentNumber(), dtoHuman.getDtoAddress().getTown(), dtoHuman.getDtoAddress().getCountry());
         return human;
@@ -12,7 +14,7 @@ public class Convertor {
 
     public DtoHuman convertToDtoHuman(Human human) {
         DtoHuman dtoHuman;
-        dtoHuman = new DtoHuman(human.getId(),human.getName(), human.getBirthDate().getYear(), human.getBirthDate().getMonth(), human.getBirthDate().getDate(),
+        dtoHuman = new DtoHuman(human.getId(), human.getName(), human.getBirthDate().getYear(), human.getBirthDate().getMonth(), human.getBirthDate().getDate(),
                 human.getAddress().getHouseNumber(), human.getAddress().getStreet(), human.getAddress().getApartmentNumber(),
                 human.getAddress().getTown(), human.getAddress().getCountry());
         return dtoHuman;
