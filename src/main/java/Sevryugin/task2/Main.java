f8dbbf628f52c9d6bb2621392745f674b36d671e
misa130 <misasevervv@mail.ru>
2020-03-19 23:54:21 +0400

    дописал валидацию

--- src/main/java/Sevryugin/task2/Main.java
+++ src/main/java/Sevryugin/task2/Main.java
@@ -3,5 +3,6 @@ package Sevryugin.task2;
 public class Main {
     public static void main(String[] args) {
         DomWriter.createXML();
+        System.out.println( MyXmlValidator.validateXml("src/main/resources/book.xsd", "books.xml"));
     }
 }
