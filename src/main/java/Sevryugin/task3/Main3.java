c439c23c7b538b5057cef7513ccd34ac583a3ca0
misa130 <misasevervv@mail.ru>
2020-03-23 22:04:03 +0400

    сделал 3 задание

---
+++ src/main/java/Sevryugin/task3/Main3.java
@@ -0,0 +1,25 @@
+package Sevryugin.task3;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main3 {
+    public static void main(String[] args) {
+        log.info(" ");
+        MyClassLoader myClassLoader = new MyClassLoader("D:\\myClasses");
+        Class<?> clazz;
+        try {
+            clazz = myClassLoader.loadClass("TestClass");
+            log.info("загружен : "+clazz.getName()+".class");
+            Object object = clazz.newInstance();
+            System.out.println(object);
+        } catch (ClassNotFoundException e) {
+            log.error("Main3 : "+e.getMessage());
+        } catch (IllegalAccessException e) {
+            log.error("Main3 : "+e.getMessage());
+        } catch (InstantiationException e) {
+            log.error("Main3 : "+e.getMessage());
+        }
+        System.out.println("конец работы программы");
+    }
+}
