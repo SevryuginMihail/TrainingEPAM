0384ccfb63254c3fc951d604632a430f6111e694
misa130 <misasevervv@mail.ru>
2020-03-26 23:41:12 +0400

    2 задание

--- src/main/java/Sevryugin/Convertor.java
+++ src/main/java/Sevryugin/Convertor.java
@@ -2,8 +2,17 @@ package Sevryugin;
 
 import lombok.Data;
 
+/**
+ * Класс реализует конвертауию между Human и DtoHuman
+ */
 @Data
 public class Convertor {
+    /**
+     * Метод конвертирует объект класса DtoHuman в объект класса Human
+     *
+     * @param dtoHuman - объект для конвертации
+     * @return - объект класса Human
+     */
     public Human convertToHuman(DtoHuman dtoHuman) {
         Human human;
         human = new Human(dtoHuman.getId(), dtoHuman.getName(), dtoHuman.getBirthDate().getYear(), dtoHuman.getBirthDate().getMonth(),
@@ -12,6 +21,12 @@ public class Convertor {
         return human;
     }
 
+    /**
+     * Метод конвертирует объект класса Human в объект класса DtoHuman
+     *
+     * @param human - объект для конвертации
+     * @return - объект класса DtoHuman
+     */
     public DtoHuman convertToDtoHuman(Human human) {
         DtoHuman dtoHuman;
         dtoHuman = new DtoHuman(human.getId(), human.getName(), human.getBirthDate().getYear(), human.getBirthDate().getMonth(), human.getBirthDate().getDate(),
