dce7bc78f37542afa80661a90a56c2216cfe137a
misa130 <misasevervv@mail.ru>
2020-04-11 21:02:28 +0400

    наметки 3 задания

---
+++ src/main/java/Sevryugin/task3/Main.java
@@ -0,0 +1,8 @@
+package Sevryugin.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        Chat chat = new Chat(25);
+
+    }
+}
---
+++ src/main/java/Sevryugin/task3/SMS_I.java
@@ -0,0 +1,5 @@
+package Sevryugin.task3;
+
+public interface SMS_I {
+    public String getMessage();
+}
