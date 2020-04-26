53197f88a88ff2bc87e6513e25461bd43770a516
misa130 <misasevervv@mail.ru>
2020-04-27 00:02:22 +0400

    Add exit from program

--- src/main/java/Sevryugin/Handler.java
+++ src/main/java/Sevryugin/Handler.java
@@ -11,5 +11,16 @@ import lombok.extern.slf4j.Slf4j;
 public class Handler {
     public void switchHandler(Integer taskNumber){
         log.info("Обработчик, задача :{}",taskNumber);
+        switch (taskNumber){
+            case 0:{exit();break;}
+            case 1:{break;}
+            case 2:{break;}
+            case 3:{break;}
+            case 4:{break;}
+            case 5:{break;}
+        }
+    }
+    public static void exit(){
+        log.info("Выход из программы");
     }
 }
