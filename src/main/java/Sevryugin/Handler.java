5a94593807ea1c0a26bb79f933265723c59094c2
misa130 <misasevervv@mail.ru>
2020-04-27 19:42:43 +0400

    Add part first task

--- src/main/java/Sevryugin/Handler.java
+++ src/main/java/Sevryugin/Handler.java
@@ -2,6 +2,10 @@ package Sevryugin;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Handler.
  *
@@ -13,7 +17,7 @@ public class Handler {
         log.info("Обработчик, задача :{}",taskNumber);
         switch (taskNumber){
             case 0:{exit();break;}
-            case 1:{break;}
+            case 1:{task1();break;}
             case 2:{break;}
             case 3:{break;}
             case 4:{break;}
@@ -22,5 +26,31 @@ public class Handler {
     }
     public static void exit(){
         log.info("Выход из программы");
+    }
+    public static void task1(){
+        Connection con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=public",
+                            "pay",
+                            "pay");
+            log.info("Соединение установлено.");
+            // тут код
+        } catch (ClassNotFoundException e) {
+            log.warn(e.getMessage());
+        } catch (SQLException e) {
+            log.warn(e.getMessage());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                    log.info("Соединение закрыто");
+                }
+            } catch (SQLException e) {
+                log.warn(e.getMessage());
+            }
+        }
+
     }
 }
--- src/main/java/Sevryugin/Main.java
+++ src/main/java/Sevryugin/Main.java
@@ -1,12 +1,16 @@
 package Sevryugin;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Main.
  *
  * @author Mihail_Sevryugin
  */
+@Slf4j
 public class Main {
     public static void main(String[] args) {
+        log.info(" ");
         Menu.menu();
     }
 }
