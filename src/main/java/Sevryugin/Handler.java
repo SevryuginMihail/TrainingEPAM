4e8a086a12bd4b765d90253f16abb3f77eabf5de
misa130 <misasevervv@mail.ru>
2020-04-27 21:55:04 +0400

    Add  task4 (delete from products where prod_id = ?)

--- src/main/java/Sevryugin/Handler.java
+++ src/main/java/Sevryugin/Handler.java
@@ -18,8 +18,8 @@ public class Handler {
             case 0:{exit();break;}
             case 1:{task1();break;}
             case 2:{task2();break;}
-            case 3:{break;}
-            case 4:{break;}
+            case 3:{task3();break;}
+            case 4:{task4();break;}
             case 5:{break;}
         }
     }
@@ -81,9 +81,7 @@ public class Handler {
                             "pay");
             log.info("Соединение установлено.");
             // начало кода
-            String request2 = "select *\n" +
-                    "from products p\n" +
-                    "where title = '" + title +"'";
+            String request2 = "select * from products p where title = '" + title +"'";
             PreparedStatement preparedStatement = con.prepareStatement(request2);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -112,7 +110,46 @@ public class Handler {
             }
         }
     }
+
     public static void task3(){
+        Connection con = null;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=public",
+                            "pay",
+                            "pay");
+            log.info("Соединение установлено.");
+            // начало кода
+            log.info("Введите id");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            log.info("Введите title");
+            String title = scanner.nextLine();
+            PreparedStatement updateTitle = con.prepareStatement("update products set title = ? where prod_id = ?;");
+            updateTitle.setString(1, title);
+            updateTitle.setInt(2, id);
+            updateTitle.execute();
+            log.info("Изминение завершено");
+            // конец кода
+        } catch (ClassNotFoundException e) {
+            log.warn(e.getMessage());
+        } catch (SQLException e) {
+            log.warn(e.getMessage());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                    log.info("Соединение закрыто");
+                }
+            } catch (SQLException e) {
+                log.warn(e.getMessage());
+            }
+        }
+    }
+
+    public static void task4(){
         Connection con = null;
         try {
             Class.forName("org.postgresql.Driver");
@@ -122,7 +159,13 @@ public class Handler {
                             "pay");
             log.info("Соединение установлено.");
             // начало кода
-
+            log.info("Введите id");
+            Scanner scanner = new Scanner(System.in);
+            int id = scanner.nextInt();
+            PreparedStatement deleteRecord = con.prepareStatement("delete from products where prod_id = ?;");
+            deleteRecord.setInt(1, id);
+            deleteRecord.execute();
+            log.info("Удаление завершено");
             // конец кода
         } catch (ClassNotFoundException e) {
             log.warn(e.getMessage());
